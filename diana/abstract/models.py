cdb6fddb06dab2524d958f268ac59b36df366678
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-05-03 11:44:50 +0200

    File system storage when saving img -> tif

--- diana/abstract/models.py
+++ diana/abstract/models.py
@@ -1,20 +1,58 @@
 from django.db import models
-from django.conf import settings
-from diana.storages import CDHDataFileStorage
+from django.core.files import File
+    
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from diana.storages import OriginalFileStorage, IIIFFileStorage
+
+from PIL import Image
+from typing import *
 import uuid
 import os
 import pyvips
 
-def get_fields(model: models.Model, exclude=['id']):
+TIFF_KWARGS = {
+    "tile": True, 
+    "pyramid": True, 
+    "compression": 'jpeg', 
+    "Q": 89, 
+    "tile_width": 256, 
+    "tile_height": 256
+}
+
+DEFAULT_FIELDS  = ['created_at', 'updated_at', 'published']
+DEFAULT_EXCLUDE = ['id', 'created_at', 'updated_at', 'published']
+
+
+def get_fields(model: models.Model, exclude=DEFAULT_EXCLUDE):
     return [field.name for field in model._meta.fields if field.name not in exclude]
 
-def get_save_path(instance: models.Model, filename, label: str):
+
+def get_media_directory(instance: models.Model, label: str):
 
     # Fetches the app name, e.g. 'arosenius'
     app = instance._meta.app_label
     
+    # Resulting directory is e.g. 'arosenius/iiif/'
+    return os.path.join(app, label)
+
+
+def get_save_path(instance: models.Model, filename, label: str):
+
+    # Fetches the closest directory
+    directory = get_media_directory(instance, label)
+    
     # Resulting directory is e.g. 'arosenius/iiif/picture.jpg'
-    return os.path.join(app, label, filename)
+    return os.path.join(directory, filename)
+
+
+def get_iiif_path(instance: models.Model, filename):
+
+    return get_save_path(instance, filename, "iiif")
+
+def get_original_path(instance: models.Model, filename):
+
+    return get_save_path(instance, filename, "original")
 
 class AbstractBaseModel(models.Model):
     """Abstract base model for all new tables in the Diana backend.
@@ -42,7 +80,7 @@ class AbstractImageModel(AbstractBaseModel):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     # The name of a supplied field is available in file.name
-    file = models.ImageField(upload_to=lambda instance, filename: get_save_path(instance, filename, "original"))
+    file = models.ImageField(storage=OriginalFileStorage, upload_to=get_original_path)
 
     class Meta:
         abstract = True
@@ -50,16 +88,15 @@ class AbstractImageModel(AbstractBaseModel):
     def __str__(self) -> str:
         return f"{self.file}"
 
-
 class AbstractTIFFImageModel(AbstractImageModel):
 
     class Meta:
         abstract = True
 
     # The path to the IIIF file
-    iiif_file = models.ImageField(upload_to=lambda instance, filename: get_save_path(instance, filename, "iiif"))
+    iiif_file = models.ImageField(storage=IIIFFileStorage, upload_to=get_iiif_path, blank=True, null=True)
 
-    def _save_tiled_pyramid_tif(self, path=CDHDataFileStorage.location):
+    def _save_tiled_pyramid_tif(self, path=IIIFFileStorage().location):
         """Uses pyvips to generate a tiled pyramid tiff.
 
         Args:
@@ -67,24 +104,37 @@ class AbstractTIFFImageModel(AbstractImageModel):
         """
 
         # The images are saved with their uuid as the key
-        out_path = os.path.join(path, get_save_path(self, str(self.uuid)+".tif", "iiif"))
+        filename = str(self.uuid) + ".tif"
+        out_path = os.path.join(path, get_iiif_path(self, filename))
 
-        image = pyvips.Image.new_from_file(self.file.path)
-        image.tiffsave(out_path, tile=True, pyramid=True, compression='jpeg', Q=89, tile_width=256, tile_height=256)
+        tmp_name = get_iiif_path(self, f"{str(self.uuid)}_tmp.tif")
+        tmp_path = os.path.join(path, tmp_name)
 
-        # Assign the new path to the IIIF file field and save again
-        self.iiif_file = out_path
-        self.save()
+
+        # When updating the file, remove the iiif_file
+        # print(out_path)
+        if os.path.isfile(out_path):
+            os.remove(out_path)
+            self.iiif_file.delete(False) # Do not yet save the image deletion
+
+        # Get the original image as a Pillow object before saving
+        image_object = Image.open(self.file.open())
+        image = pyvips.Image.new_from_array(image_object)
+                
+        # Create temporary file
+        image.tiffsave(tmp_path, **TIFF_KWARGS)
+
+        # Prepare saving the new IIIF file
+        with open(tmp_path, 'rb') as f:
+            tiff_image = File(f) 
+            self.iiif_file.save(filename, tiff_image, save=False)
         
+        # Remove the temporary path
+        if os.path.isfile(tmp_path):
+            os.remove(tmp_path)
 
-    def save(self, *args, **kwargs) -> None:
-        """Redefines the save protocol to ensure that a .tif copy is created 
-        at the IIIF server destination. The original file is still saved in the media location.
-        """
+    def save(self, **kwargs) -> None:
 
-        success = super().save(*args, **kwargs)
-
-        # Call the saving routine
         self._save_tiled_pyramid_tif()
 
-        return success
+        super().save(**kwargs)
--- diana/storages.py
+++ diana/storages.py
@@ -1,10 +1,18 @@
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 
-class CDHDataFileStorage(FileSystemStorage):
+class OriginalFileStorage(FileSystemStorage):
     def __init__(self,) -> None:
 
         location = settings.MEDIA_ROOT
-        base_url = settings.MEDIA_URL
+        base_url = settings.ORIGINAL_URL
+
+        super().__init__(location, base_url)
+
+class IIIFFileStorage(FileSystemStorage):
+    def __init__(self,) -> None:
+
+        location = settings.MEDIA_ROOT
+        base_url = settings.IIIF_URL
 
         super().__init__(location, base_url)
