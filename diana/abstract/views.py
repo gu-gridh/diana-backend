8f91921724710d09588a9ec822fc39ebb930d069
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-06-09 10:09:26 +0200

    Added support for dynamic api

--- diana/abstract/views.py
+++ diana/abstract/views.py
@@ -1,9 +1,30 @@
-from rest_framework import viewsets, generics, mixins
+from rest_framework import viewsets, generics, mixins, pagination
 from django_filters.rest_framework import DjangoFilterBackend
 from . import schemas, serializers
 
-class GenericReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
+
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+class CountModelMixin(object):
+    """
+    Count a queryset.
+    """
+    @action(detail=False)
+    def count(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        content = {'count': queryset.count()}
+        return Response(content)
+
+
+class GenericPagination(pagination.PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
+
+class GenericModelViewSet(viewsets.ModelViewSet, CountModelMixin):
 
     filter_backends = [DjangoFilterBackend]
     filterset_fields = '__all__'
-    schema  = schemas.MetaDataSchema()
+    pagination_class = GenericPagination
+    schema = schemas.AutoSchema()
--- diana/utils.py
+++ diana/utils.py
@@ -1,7 +1,7 @@
 import json
 from typing import *
 from django.apps import apps
-
+from django.urls import re_path
 from diana.abstract import serializers, views
 
 def read_json(path: str) -> Dict:
@@ -9,30 +9,41 @@ def read_json(path: str) -> Dict:
     with open(path, 'r') as f:
         return json.load(f)
 
-def get_read_only_urls(app_label: str, base_url: str, exclude: List[str]):
+
+def get_serializer(model):
+
+    serializer = serializers.GenericSerializer
+    serializer.Meta.model = model
+
+    return serializer
+
+def get_model_urls(app_label: str, base_url: str, exclude: List[str]):
 
     # Fetch the application, with registered models
     app = apps.get_app_config(app_label)
-
-    url_configs = []
+    patterns = []
 
     for model_name, model in app.models.items():
 
-        # Get the url of the specific model
-        url = rf'{base_url}/{model_name}'
+        urls = {
+            'list': rf'{base_url}/{model_name}/?$',
+            'retrieve': rf'{base_url}/{model_name}/<int:pk>/?$',
+            'count': rf'{base_url}/{model_name}/count/?$',
+        }
 
-        if model_name not in exclude:
-            
-            # Add a viewset
-            viewset = views.GenericReadOnlyModelViewSet
-            viewset.queryset = model.objects.all()
+        for action, url in urls.items():
 
-            # Add a basic serializer
-            serializer = serializers.GenericSerializer
-            serializer.Meta.model = model
+            if model_name not in exclude:
+                
+                patterns.append(
+                    re_path(
+                        url, 
+                        views.GenericModelViewSet.as_view(actions={'get': action}, 
+                        queryset=model.objects.all(), 
+                        serializer_class=get_serializer(model)), 
+                        {'model': model}
+                        )
+                    )
 
-            viewset.serializer_class = serializer
-            
-            url_configs.append({'prefix': url, 'viewset': viewset, 'basename': model_name})
+    return patterns
 
-    return url_configs
