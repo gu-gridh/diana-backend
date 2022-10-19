bd2fd1bdd866012ad46481d2515e332df6361011
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-10-19 11:08:59 +0200

    Added verbose naming and reformat

--- diana/abstract/models.py
+++ diana/abstract/models.py
@@ -2,7 +2,7 @@
 from django.contrib.gis.db import models
 
 from django.core.files import File
-    
+from django.utils.translation import gettext_lazy as _
 from django.contrib.postgres.search import SearchVectorField
 from django.contrib.postgres.indexes import GinIndex 
 from diana.storages import OriginalFileStorage, IIIFFileStorage
@@ -40,7 +40,6 @@ def get_media_directory(instance: models.Model, label: str):
     # Resulting directory is e.g. 'arosenius/iiif/'
     return os.path.join(app, label)
 
-
 def get_save_path(instance: models.Model, filename, label: str):
 
     # Fetches the closest directory
@@ -57,6 +56,51 @@ def get_iiif_path(instance: models.Model, filename):
 def get_original_path(instance: models.Model, filename):
 
     return get_save_path(instance, filename, "original")
+
+
+def save_tiled_pyramid_tif(obj, path=IIIFFileStorage().location):
+    """Uses pyvips to generate a tiled pyramid tiff.
+
+        Args:
+            path (str, optional): The path to save the images. Defaults to IIIF_PATH.
+        """
+
+    # Create directory if it does not exist
+    save_path = os.path.join(path, get_iiif_path(obj, ""))
+    if not os.path.exists(save_path):
+        os.makedirs(save_path) 
+
+    # The images are saved with their uuid as the key
+    filename = str(obj.uuid) + ".tif"
+    out_path = os.path.join(path, get_iiif_path(obj, filename))
+
+    tmp_name = get_iiif_path(obj, f"{str(obj.uuid)}_tmp.tif")
+    tmp_path = os.path.join(path, tmp_name)
+
+
+    # When updating the file, remove the iiif_file
+    # print(out_path)
+    if os.path.isfile(out_path):
+        os.remove(out_path)
+        obj.iiif_file.delete(False) # Do not yet save the image deletion
+
+    # Get the original image as a Pillow object before saving
+    image_object = Image.open(obj.file.open())
+    image = pyvips.Image.new_from_array(image_object)
+                
+    # Create temporary file
+    image.tiffsave(tmp_path, **TIFF_KWARGS)
+
+    # Prepare saving the new IIIF file
+    with open(tmp_path, 'rb') as f:
+        tiff_image = File(f) 
+        obj.iiif_file.save(filename, tiff_image, save=False)
+        
+    # Remove the temporary path
+    if os.path.isfile(tmp_path):
+        os.remove(tmp_path)
+
+    return path
 
 
 #####################################################
@@ -76,9 +120,9 @@ class AbstractBaseModel(models.Model):
     as well as a toggle for publication.
     """
     
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    published  = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose=_("abstract.created_at"))
+    updated_at = models.DateTimeField(auto_now=True, verbose=_("abstract.updated_at"))
+    published  = models.BooleanField(default=True, verbose=_("abstract.published"))
 
     class Meta:
         abstract = True
@@ -90,7 +134,7 @@ class AbstractBaseModel(models.Model):
 class AbstractTagModel(AbstractBaseModel):
     """Abstract model which creates a simple tag with a case-insensitive text field.
     """
-    text = CINameField(max_length=256)
+    text = CINameField(max_length=256, unique=True, verbose=_("general.text"))
 
     class Meta:
         abstract = True
@@ -111,7 +155,7 @@ class AbstractImageModel(AbstractBaseModel):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     # The name of a supplied field is available in file.name
-    file = models.ImageField(storage=OriginalFileStorage, upload_to=get_original_path)
+    file = models.ImageField(storage=OriginalFileStorage, upload_to=get_original_path, verbose=_("general.file"))
 
     class Meta:
         abstract = True
@@ -130,48 +174,12 @@ class AbstractTIFFImageModel(AbstractImageModel):
         abstract = True
 
     # The path to the IIIF file
-    iiif_file = models.ImageField(storage=IIIFFileStorage, upload_to=get_iiif_path, blank=True, null=True)
-
-    def _save_tiled_pyramid_tif(self, path=IIIFFileStorage().location):
-        """Uses pyvips to generate a tiled pyramid tiff.
-
-        Args:
-            path (str, optional): The path to save the images. Defaults to IIIF_PATH.
-        """
-
-        # The images are saved with their uuid as the key
-        filename = str(self.uuid) + ".tif"
-        out_path = os.path.join(path, get_iiif_path(self, filename))
-
-        tmp_name = get_iiif_path(self, f"{str(self.uuid)}_tmp.tif")
-        tmp_path = os.path.join(path, tmp_name)
-
-
-        # When updating the file, remove the iiif_file
-        # print(out_path)
-        if os.path.isfile(out_path):
-            os.remove(out_path)
-            self.iiif_file.delete(False) # Do not yet save the image deletion
-
-        # Get the original image as a Pillow object before saving
-        image_object = Image.open(self.file.open())
-        image = pyvips.Image.new_from_array(image_object)
-                
-        # Create temporary file
-        image.tiffsave(tmp_path, **TIFF_KWARGS)
-
-        # Prepare saving the new IIIF file
-        with open(tmp_path, 'rb') as f:
-            tiff_image = File(f) 
-            self.iiif_file.save(filename, tiff_image, save=False)
-        
-        # Remove the temporary path
-        if os.path.isfile(tmp_path):
-            os.remove(tmp_path)
+    iiif_file = models.ImageField(storage=IIIFFileStorage, upload_to=get_iiif_path, blank=True, null=True, verbose=_("abstract.iiif_file"))
 
     def save(self, **kwargs) -> None:
 
-        self._save_tiled_pyramid_tif()
+        # self._save_tiled_pyramid_tif()
+        save_tiled_pyramid_tif(self)
 
         super().save(**kwargs)
 
@@ -189,13 +197,13 @@ class AbstractDocumentModel(AbstractBaseModel):
     uuid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
 
     # The textual content
-    text    = models.TextField(default="")
+    text    = models.TextField(default="", verbose=_("general.text"))
 
     # The text vector is a generated column which holds
     # tokenized versions of all columns which should be searchable
     # Performance is vastly improved if accompanied by a manual migration 
     # which adds this column automatically, instead of at runtime
-    text_vector = SearchVectorField(null=True)
+    text_vector = SearchVectorField(null=True, verbose=_("abstract.text_vector"))
 
     class Meta:
         abstract = True
