b8c22bee2cdde68534cb21d2b52fc5a65522598c
Victor Wåhlstrand Skärström <victor.wahlstrand@gmail.com>
2022-10-25 13:26:02 +0200

    Updated abstract REST

--- diana/abstract/serializers.py
+++ diana/abstract/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from drf_dynamic_fields import DynamicFieldsMixin
 
-class GenericSerializer(serializers.ModelSerializer):
+class GenericSerializer(serializers.ModelSerializer, DynamicFieldsMixin):
 
     class Meta:
         model = None
         fields = '__all__'
         depth = 1
+
+class DynamicDepthSerializer(GenericSerializer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.Meta.depth = self.context.get('depth', 0)
+
--- diana/abstract/views.py
+++ diana/abstract/views.py
@@ -3,6 +3,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, pagination
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework_gis.filters import InBBoxFilter
+from rest_framework_gis.pagination import GeoJsonPagination
+
 
 from . import schemas
 
@@ -17,12 +20,16 @@ class CountModelMixin(object):
         content = {'count': queryset.count()}
         return Response(content)
 
-
 class GenericPagination(pagination.LimitOffsetPagination):
     """
     The pagination of choice is limit-offset pagination.
     """
     default_limit = 25
+
+class GeoJsonPagePagination(GeoJsonPagination):
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 20
 
 class GenericModelViewSet(viewsets.ModelViewSet, CountModelMixin):
     """
@@ -33,3 +40,32 @@ class GenericModelViewSet(viewsets.ModelViewSet, CountModelMixin):
     filterset_fields = '__all__'
     pagination_class = GenericPagination
     schema = schemas.AutoSchema()
+
+
+class DynamicDepthViewSet(GenericModelViewSet):
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        depth = 0
+        try:
+            depth = int(self.request.query_params.get('depth', 0))
+            print(depth)
+        except ValueError:
+            pass # Ignore non-numeric parameters and keep default 0 depth
+        
+        context['depth'] = depth
+
+        return context
+
+class GeoViewSet(GenericModelViewSet):
+
+    filter_backends = [InBBoxFilter, DjangoFilterBackend]
+    schema = schemas.AutoSchema()
+    
+    # GIS filters
+    # Default field name
+    bbox_filter_field = 'geometry'
+
+    # Specialized pagination
+    pagination_class = GeoJsonPagePagination
+    page_size = 10
