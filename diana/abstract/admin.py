da421526a6375d3f63902f65b7619d493d0c8e02
AramKarimi <aram.karimi@gu.se>
2023-02-07 12:01:01 +0100

    update admin page and shfa ordering for admin page

--- diana/abstract/admin.py
+++ diana/abstract/admin.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin, sites
 from .admin_view import dict
 
-def admin_view_site(apps_order):
+
+def admin_view_site():
     def get_app_list(self, request):
         """
         Return a sorted list of all the installed apps that have been
@@ -13,13 +14,14 @@ def admin_view_site(apps_order):
         app_list = sorted(app_dict.values(), key=lambda x: x['name'])
         # Sort the models alphabetically within each app.
         for app in app_list:
-            if app['name'] in apps_order:
+            if app['name'] in dict:
                 # app['models'] = apps_order[app['name']]
-                app['models'].sort(key=lambda x: apps_order[app['name']].index(x['name']))
+                app['models'].sort(key=lambda x: dict[app['name']].index(x['name']))
             else:
                 app['models'].sort(key=lambda x: x['name'])
 
         return app_list
     return get_app_list
 
-admin.AdminSite.get_app_list = admin_view_site(dict)
+
+admin.AdminSite.get_app_list = admin_view_site()
