d27bdd419a604d5776e847103dc238d63a335d20
AramKarimi <aram.karimi@gu.se>
2025-02-14 07:01:30 +0100

    update contact form

--- diana/views.py
+++ diana/views.py
@@ -1,32 +1,32 @@
-from django.http import JsonResponse
-from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+
 from django.core.mail import send_mail
 from django.conf import settings
-import json
+from rest_framework import viewsets
+from .forms import ContactForm  # Import ContactForm
+from rest_framework.response import Response
+from rest_framework import status
 
-@csrf_exempt
-def contact(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            name = data.get('name')
-            email = data.get('email')
-            subject = data.get('subject')
-            message = data.get('message')
-
-            if not all([name, email, subject, message]):
-                return JsonResponse({'error': 'Missing required fields'}, status=400)
-
-            send_mail(
-                f'From {name}, Subject: {subject}',
-                f'Message: {message}\n',
-                email,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=False,
-            )
-            return JsonResponse({'message': 'Email sent successfully'})
-        except json.JSONDecodeError:
-            return JsonResponse({'error': 'Invalid JSON'}, status=400)
-    
-    return JsonResponse({'error': 'Invalid request method'}, status=405)
+# Add contact form view
+class ContactFormViewSet(viewsets.ViewSet):
+    def create(self, request):
+        if request.method == 'POST':
+            form = ContactForm(request.POST)
+            if form.is_valid():
+                # Process the form data
+                name = form.cleaned_data['name']
+                email = form.cleaned_data['email']
+                subject = form.cleaned_data['subject']
+                message = form.cleaned_data['message']
+                
+                # Send an email
+                send_mail(
+                    f'From {name}, Subject: {subject}',
+                    f'Message: {message}\n',
+                    email,  # From email
+                    [settings.EMAIL_HOST_USER],  # To email
+                    fail_silently=False,
+                )
+            return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+        else:
+            form = ContactForm()
+        return Response({'error': 'Invalid request method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
