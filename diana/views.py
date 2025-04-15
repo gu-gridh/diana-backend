c2e031c1fd74d633a17654d8d8303fc8ec7c0fed
AramKarimi <aram.karimi@gu.se>
2025-04-15 16:06:54 +0200

    update contact from code

--- diana/views.py
+++ diana/views.py
@@ -6,27 +6,21 @@ from .forms import ContactForm  # Import ContactForm
 from rest_framework.response import Response
 from rest_framework import status
 
-# Add contact form view
 class ContactFormViewSet(viewsets.ViewSet):
     def create(self, request):
-        if request.method == 'POST':
-            form = ContactForm(request.POST)
-            if form.is_valid():
-                # Process the form data
-                name = form.cleaned_data['name']
-                email = form.cleaned_data['email']
-                subject = form.cleaned_data['subject']
-                message = form.cleaned_data['message']
-                
-                # Send an email
-                send_mail(
-                    f'From {name}, Subject: {subject}',
-                    f'Message: {message}\n',
-                    email,  # From email
-                    [settings.EMAIL_HOST_USER],  # To email
-                    fail_silently=False,
-                )
+        form = ContactForm(request.data)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            
+            send_mail(
+                f'From {name}, Subject: {subject}',
+                f'Message: {message}\n',
+                email,
+                [settings.EMAIL_HOST_USER],
+                fail_silently=False,
+            )
             return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
-        else:
-            form = ContactForm()
-        return Response({'error': 'Invalid request method'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return Response({'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
