e0c6ffc0a0d28057df1a59f4b373140e9ad530a4
AramKarimi <aram.karimi@gu.se>
2025-04-16 14:27:30 +0200

    update contact form

--- diana/views.py
+++ diana/views.py
@@ -8,19 +8,20 @@ from rest_framework import status
 
 class ContactFormViewSet(viewsets.ViewSet):
     def create(self, request):
-        form = ContactForm(request.data)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            subject = form.cleaned_data['subject']
-            message = form.cleaned_data['message']
-            
-            send_mail(
-                f'From {name}, Subject: {subject}',
-                f'Message: {message}\n',
-                email,
-                [settings.EMAIL_HOST_USER],
-                fail_silently=False,
-            )
-            return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
+        if request.method == 'POST':
+            form = ContactForm(request.data)
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                email = form.cleaned_data['email']
+                subject = form.cleaned_data['subject']
+                message = form.cleaned_data['message']
+                
+                send_mail(
+                    f'From {name}, Subject: {subject}',
+                    f'Message: {message}\n',
+                    email,
+                    [settings.EMAIL_HOST_USER],
+                    fail_silently=False,
+                )
+                return Response({'message': 'Email sent successfully'}, status=status.HTTP_200_OK)
         return Response({'errors': form.errors}, status=status.HTTP_400_BAD_REQUEST)
